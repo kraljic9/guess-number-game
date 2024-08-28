c1a07005098626405857bd4fc1f5c4c53aaee876
Pavle Kraljic <kraljicpavle9@gmail.com>
2024-08-28 15:44:24 +0200

    added game logic for winning and loosing the game

--- README.md
+++ README.md
@@ -17,7 +17,9 @@
 - Guess click event
 - Score decresse if guess is wrong
 - Keep higscore
-- Make secret number go from 0 to 20
+- Make secret number go from 0 to 20 - OK!
 - Text change if guess is higher or lower than secret number
 - Win screen if you guess correct
 - Loose screen if you run out of points
+
+- Logic of game when guess button is clicked if number input is empty it should ask for a input again, if number === secret number games stops and winner message and screen appears, if number !== secret number it says if number is less or more than secret number game is played untill score is less than 1 and loosing screen apears.
--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 "use strict";
+
+// secret number - .number
+// check button - .check
+// guess number input - .guess
+// message for feedback - .message
+// score - .score
+// highscore - .highscore
+
+let score = 20;
+let highscore = 0;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+document.querySelector(".check").addEventListener("click", function () {
+  let guess = Number(document.querySelector("#guess").value); // Number is used to turn input into a number, .value to read the value of input
+
+  // IF THERE IS NO NUMBER INPUT
+  if (!guess) {
+    document.querySelector(".message").textContent = "Input Number Please";
+  } else if (guess === secretNumber) {
+    // WINNING SCREEN
+    document.querySelector("body").style.backgroundColor = "#60b347";
+    document.querySelector(".message").textContent = "You Win!";
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector(".highscore").textContent = highscore;
+    }
+  } else if (guess < secretNumber) {
+    // LOOSING SCREEN
+    score--;
+    document.querySelector(".message").textContent = "Guess Higher";
+    document.querySelector(".score").textContent = score;
+    if (score < 1) {
+      document.querySelector("body").style.backgroundColor = "#FF474C";
+      document.querySelector(".message").textContent = "LOSER";
+    }
+  } else if (guess > secretNumber) {
+    score--;
+    document.querySelector(".message").textContent = "Guess Lower";
+    document.querySelector(".score").textContent = score;
+    if (score < 1) {
+      document.querySelector("body").style.backgroundColor = "#FF474C";
+      document.querySelector(".message").textContent = "LOSER";
+    }
+  }
+});
